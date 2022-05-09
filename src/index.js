f36e70951c72252c287804de73ebcd90830a4fbf
Francisco Mesar <fmesar87@gmail.com>
2022-05-08 20:15:01 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,65 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+document.querySelector('.add-toy-form').addEventListener('submit',
+handleSubmit)
+function handleSubmit(e){
+  e.preventDefault()
+  let toyObj = {
+  name:e.target.name.value,
+  image:e.target.image.value,
+  likes: 0
+  }
+  addToys(toyObj)
+}
+function renderCaracters(carcter){
+let card = document.createElement('div')
+  card.className = 'card'
+  card.innerHTML = `
+      <h2>${carcter.name}</h2>
+      <img src="${carcter.image}"  class="toy-avatar" />
+      <p>
+      <span class = "likes-count">${carcter.likes} </span> Likes </p>
+      <button class="like-btn" id="[toy_id]">Like</button>
+      `
+    card.querySelector('.like-btn').addEventListener('click',() => {
+      carcter.likes+= 1
+      card.querySelector('span').textContent = carcter.likes
+    } )
+      document.querySelector('#toy-collection').appendChild(card)
+}
+
+function getAllToys(){
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(toyData => toyData.forEach(toy => renderCaracters(toy) ))
+  
+}
+function addToys(toyObj){
+  fetch('http://localhost:3000/toys',{
+    method: 'POST',
+    headers: 
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body:JSON.stringify(toyObj)
+  })
+  .then(res => res.json())
+  .then(toy => console.log(toy))
+}
+function updateToys(toyObj){
+  fetch(`http://localhost:3000/toys/${toyObj.id}`,{
+    method: 'PATCH',
+    headers: 
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body:JSON.stringify(toyObj)
+  })
+  .then(res => res.json())
+  .then(toy => console.log(toy))
+}
+
+getAllToys()
